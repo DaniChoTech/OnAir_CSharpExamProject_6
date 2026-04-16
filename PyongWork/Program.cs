665a5f0bd624d79d405cb45277772f6e3e9b5362
danielcho <danitech171791@gmail.com>
2026-04-16 15:50:42 +0900

    스탭 2) 플레이어와 몬스터 (즉, 게임의 주인공이 되는 대상들을 미리 만들고, 관련 기능을 정의하고) 실체화

--- PyongWork/Program.cs
+++ PyongWork/Program.cs
@@ -10,6 +10,40 @@ namespace PyongWork
     {
         static bool _isGameOver = false;
 
+        public class PugPlayer
+        {
+            public string Name { get; private set; }
+            public int Hp { get; private set; }
+
+            public void InitPugPlayer(string name, int hp)
+            {
+                this.Name = name;
+                this.Hp = hp;
+            }
+
+            public void AttackMonster()
+            {
+            }
+        }
+
+
+        public class Monster
+        {
+            public string Name { get; private set; }
+            public int Hp { get; private set; }
+
+            public void InitMonster(string name, int hp)
+            {
+                this.Name = name;
+                this.Hp = hp;
+            }
+
+            public void AttackPlayer()
+            {
+            }
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("던전에 진입했다...두두둥 더 들어가시겠습니까? 안전은 보장할 수 없습니다 입장 : 1, 퇴장 : 2");
@@ -21,8 +55,16 @@ namespace PyongWork
                 return;
             }
 
+            PugPlayer player = new PugPlayer();
+            player.InitPugPlayer("퍼그용사", 100);
+            Console.WriteLine($"{player.Name}가 입장했다!");
 
-            while(_isGameOver == false) 
+            Monster mob103 = new Monster();
+            mob103.InitMonster("슬라임", 300);
+
+
+
+            while (_isGameOver == false) 
             {
                 Console.WriteLine("...계단이 나왔다 더 들어갈 것인가! 내려간다 1, 퇴장한다 2");
                 string repeatKey = Console.ReadLine();
